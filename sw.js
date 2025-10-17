08767471b5a527bbf55aea5012973d76c750173d
Sumant Deshmukh <ssd9890880131@gmail.com>
2025-10-17 13:27:39 +0530

    Initial commit of student attendance system

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = 'student-attendance-cache-v1';
+const urlsToCache = [
+  '/',
+  'student_app.html',
+  'teacher_dashboard.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
